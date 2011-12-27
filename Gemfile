245a054f6a47bcaa1114487e42fb8944c5e54be7
Rahul Sekhar <rahul@rahul-ubuntu.(none)>
2011-12-27 22:06:38 +0530

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'cancan'
+gem 'sanitize'
+gem 'squeel'
+gem 'yaml_db'
+gem 'valid_email', :require => 'valid_email/email_validator'
+gem 'kaminari'
+gem 'carrierwave'
+gem 'rmagick'
+
+gem 'capistrano'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoryRev::Application.config.secret_token = 'bb0349206340572d0ec6bd19cd89e999cc5bf2c3918a40be67538b32eb67efc40bc6dee424890a10b507740976492dae0ca843ff2863d5cec438a9cce78974f6'
