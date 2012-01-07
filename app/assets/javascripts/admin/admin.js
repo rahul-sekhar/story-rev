bdebcb1ac84e39d2de9adeedb835c3d93037ca0c
Rahul Sekhar <sekhar.rahul@gmail.com>
2012-01-07 22:06:10 +0530

    Minor correction

--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -4,7 +4,7 @@
 //= require jquery_ujs
 //= require_directory ../lib
 //= require_directory ../init
-//= require_directory lib
-//= require_directory init
+//= require_directory ./lib
+//= require_directory ./init
 //= require_directory .
 //
