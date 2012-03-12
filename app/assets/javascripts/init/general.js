1f7271063d411a39f3fdd1fbc61f4f4491ec6091
Rahul Sekhar <sekhar.rahul@gmail.com>
2012-03-12 17:50:20 +0530

    Added footer, made some css files common

--- app/assets/javascripts/init/general.js
+++ app/assets/javascripts/init/general.js
@@ -1,9 +1,37 @@
-// Handle external links
 $(document).ready(function() {
+    // Handle external links
     $("a.ext").click(function(e) {
         window.open(this.href);
         e.preventDefault();
     });
+    
+    // Email obfuscation [delay for a second in case that helps with spam]
+    var $emailLinks = $('.email_obf');
+    if ($emailLinks.length) {
+        setTimeout(function() {
+            
+            // Email obfuscator script 2.1 by Tim Williams, University of Arizona
+            // Random encryption key feature by Andrew Moulden, Site Engineering Ltd
+            // This code is freeware provided these four comment lines remain intact
+            // A wizard to generate this code is at http://www.jottings.com/obfuscator/
+            coded = "2avini8Qv4baEv3@WhLE4.Pvh"
+            key = "7wjzyc2dJeNCHZhLiBD59MAv4S0tFnPugY86G1UXKExsfIpRQTkmar3qbWOVol"
+            shift = coded.length;
+            link = "";
+            for (i=0; i<coded.length; i++) {
+                if (key.indexOf(coded.charAt(i))==-1) {
+                    ltr = coded.charAt(i);
+                    link += (ltr);
+                }
+                else {     
+                    ltr = (key.indexOf(coded.charAt(i)) - shift+key.length) % key.length;
+                    link += (key.charAt(ltr));
+                }
+            }
+            
+            $emailLinks.html('<a href="mailto:' + link + '">' + link + '</a>');
+        }, 1000);
+    }
 });
 
 // HTML Escaping function
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,29 @@
 module ApplicationHelper
   
+  # Add parameters to the current URL
+  def current_url(new_params)
+    new_params = request.query_parameters.merge(new_params)
+    request.url.split("?")[0] + "?" + new_params.map{ |k,v| "#{k}=#{v}" }.join("&")
+  end
+  
+  # The more info menu items
+  def more_nav_links
+    {
+      "books-special" => "Why we think these books are special",
+      "condition" => "Condition of used books",
+      "ordering" => "Ordering",
+      "delivery" => "Delivery",
+      "payment" => "Payment",
+      "story-revolution" => "Story Revolution"
+    }
+  end
+  
+  def more_nav_selected
+    @more_nav_selected ||= more_nav_links.has_key?(params["info"]) ? params[:info] : more_nav_links.first[0]
+  end
+  
+  def class_if (condition, class_name)
+    (' class="' + class_name + '"').html_safe if (condition)
+  end
+  
 end
