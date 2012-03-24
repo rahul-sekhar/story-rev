f77f4266eea89bbd1c082fecddb832b4b74caed3
Rahul Sekhar <sekhar.rahul@gmail.com>
2012-03-24 18:51:09 +0530

    Minor change

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -228,7 +228,7 @@ $(document).ready(function() {
                 shoppingCartSwitchHtml(data.html);
                 updateShoppingCartCount(data.item_count);
                 
-                if (callbackFunction) successFunction();
+                if (successFunction) successFunction();
             },
             error: function(xhr) {
                 $shoppingCartSection.empty().append($removedHtml.contents());
