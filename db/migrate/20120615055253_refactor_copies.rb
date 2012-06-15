533526e0d062d1f2ce370df4ec5cb14df244434d
Rahul Sekhar <rahul@kairi.in>
2012-06-15 16:48:13 +0530

    Copy refactor migration

---
+++ db/migrate/20120615055253_refactor_copies.rb
@@ -0,0 +1,22 @@
+class RefactorCopies < ActiveRecord::Migration
+  def change
+    create_table :used_copies do |t|
+      t.integer :edition_id
+      t.integer :copy_number
+      t.string :condition_description
+      t.integer :condition_rating, :limit => 2
+      t.integer :price
+      t.integer :order_id
+      t.timestamps
+    end
+    
+    create_table :new_copies do |t|
+      t.integer :edition_id
+      t.integer :copy_number
+      t.integer :stock
+      t.integer :price
+      t.integer :required_stock
+      t.timestamps
+    end
+  end
+end
