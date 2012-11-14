66e8da7a201394303bfc8b37675def7188c7444a
Rahul Sekhar <rahul@kairi.in>
2012-11-14 21:47:31 +0530

    Changed redirect after book edit

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -13,7 +13,7 @@ class Admin::BooksController < Admin::ApplicationController
     respond_to do |format|
       format.html
       format.json do
-        render :json => BookSearcher.search(params[:q]).formatted_results
+        render json: BookSearcher.search(params[:q]).formatted_results
       end
     end
   end
@@ -36,10 +36,10 @@ class Admin::BooksController < Admin::ApplicationController
   def create
     @book = Book.new(params[:book])
     if @book.save
-      redirect_to admin_book_path(@book), :notice => "Book created - its accession number is #{@book.accession_id}"
+      redirect_to admin_book_path(@book), notice: "Book created - its accession number is #{@book.accession_id}"
     else
       flash[:book_params] = params[:book]
-      redirect_to :action => :new
+      redirect_to action: :new
     end
   end
   
@@ -63,27 +63,27 @@ class Admin::BooksController < Admin::ApplicationController
       when "Previous Book"
         redirect_path = edit_admin_book_path(@book.previous_book)
       else
-        redirect_path = search_admin_books_path
+        redirect_path = admin_book_path(@book)
       end
       
-      redirect_to redirect_path, :notice => "Book saved - its accession number is #{@book.accession_id}"
+      redirect_to redirect_path, notice: "Book saved - its accession number is #{@book.accession_id}"
     else
       flash[:book_params] = params[:book]
-      redirect_to :action => :edit
+      redirect_to action: :edit
     end
   end
   
   def destroy
     @book = Book.find_by_accession_id!(params[:id])
     @book.destroy
-    redirect_to search_admin_books_path, :notice => "Book #{@book.accession_id} - #{@book.title} has been deleted"
+    redirect_to search_admin_books_path, notice: "Book #{@book.accession_id} - #{@book.title} has been deleted"
   end
   
   def amazon_info
     @info_objects = AWSInfo.search(params[:title])
     
     respond_to do |format|
-      format.json { render :json => @info_objects }
+      format.json { render json: @info_objects }
     end
   end
 end
