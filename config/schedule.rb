812fb772079437da65ff7d865c601c98a58f76fa
Rahul Sekhar <sekhar.rahul@gmail.com>
2013-07-04 12:05:44 +0530

    start delayed job automatically

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,27 +1,12 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
-
 set :output, "log/cron.log"
 
 job_type :runner,  "cd :path && bundle exec rails runner -e :environment ':task' :output"
+job_type :envcommand, 'cd :path && RAILS_ENV=:environment :task'
+
+# Start delayed job on reboot
+every :reboot do
+  envcommand 'script/delayed_job restart'
+end
 
 # Daily backup
 every 1.day, :at => '4:00 am' do
