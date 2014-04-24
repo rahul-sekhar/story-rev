b7f50f36a71e9d4c287ed3df7fda9e5c9f1bdef0
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-04-24 10:25:58 +0530

    associate tag to sesitive data

--- config/application.rb
+++ config/application.rb
@@ -14,10 +14,10 @@ module StoryRev
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    
+
     # Load sensitive data from a YAML file
     config.sensitive = YAML.load_file("#{config.root}/config/sensitive.yml")
-  
+
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
 
@@ -31,7 +31,7 @@ module StoryRev
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Chennai'
-    
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
