c7ef01bbc3061b92e93e27584e85e515d4b20265
Rahul Sekhar <sekhar.rahul@gmail.com>
2017-07-26 11:05:40 +0530

    Added export

---
+++ lib/export_books.rb
@@ -0,0 +1,20 @@
+module ExportBooks
+  def self.to_csv(copies)
+    CSV.generate(headers: true) do |csv|
+      csv << %w{Title Author Price New Condition Age-From Age-To Stock} 
+
+      copies.each do |copy|
+        csv << [
+          copy.book.title,
+          copy.book.author_name,
+          copy.price,
+          copy.new_copy,
+          copy.condition_rating,
+          copy.book.age_from
+          copy.book.age_to
+          copy.stock
+        ];
+      end
+    end
+  end
+end
